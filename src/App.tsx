6ab120a24598b0784f6e4c1c2eff46b44d4dfd5d
hanglong4000 <sapbatcheung@gmail.com>
2022-11-06 00:38:32 +0700

    second turn

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import vearn from "./assets/dark-king-vearn.png";
+import kangLiu from "./assets/revenant_kang_liu_1.png";
 
 function App() {
   const [count, setCount] = useState(0);
   const [number, setNumber] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="vite" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={vearn} className="logo" alt="React logo" />
-        </a>
+    <>
+      <div className="App">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="vite" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={kangLiu} className="logo" alt="React logo" />
+          </a>
+        </div>
+
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,10 +35,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   );
 }
 

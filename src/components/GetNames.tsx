af5b9062e88ac11017b5578d33f3513db8b1b887
hanglong4000 <sapbatcheung@gmail.com>
2022-11-15 10:49:43 +0700

    I deleted the clickbait file, mwa ha ha ha ha ha ha

--- src/components/GetNames.tsx
+++ src/components/GetNames.tsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
-
 
 type Props = {};
 
 const GetNames = (props: Props) => {
+  const [names, setNames] = useState("Hung");
+
   const handleName = () => {
     const names = ["Hung", "Hoang", "Minh", "Trang", "Hien", "Thuy Linh"];
     const int = Math.floor(Math.random() * 6);
-    return names[int];
+    setNames(names[int]);
+  };
+
+  const handleClick = (e: any) => {
+    // alert(e.target.innerText);
+    alert ("Hah, you suckers. You clicked on it. It was a clickbait to bait you, mwa ha ha ha ha ha")
   };
 
   return (
     <>
       <div className="container">
-        <p>Hello {handleName()}</p>
+        <p>Hello {names}</p>
         {/* The "handleName" function generates the name of each person within the array that I created         */}
+        <button onClick={handleName}>Change the name</button>
+        {/* When I click on this button, the name will change  */}
+        <button onClick={handleClick}>Show the alert</button>
       </div>
     </>
   );
